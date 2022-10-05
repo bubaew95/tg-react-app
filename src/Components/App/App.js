485962ef1b38409a98659c24cf82f37c21a5457a
manager <manager@Users-MacBook-Pro.local>
2022-10-06 02:03:13 +0300

    update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,62 +10,80 @@ const goods = [
     "id": 1,
     "name": "Костюм Ailooge",
     "img": "https://ae01.alicdn.com/kf/HTB1RltMRVXXXXafXFXXq6xXFXXXY.jpg",
-    "price": "2 566"
+    "price": "2566"
   },
   {
     "id": 2,
    "name": "Nova Moda Dos Homens T De Manga",
     "img": "https://www.ofertaviva.com.br/photo/nova-moda-dos-homens-t-de-manga-longa-henley-camisas-militares-casuais-t-shirt-top-roupas-masculinas-camiseta-de-lycra-de-algoda.jpg",
-    "price": "1 599"
+    "price": "1599"
   },
   {
     "id": 3,
     "name": "Casuais Dois Botões Dos Homens",
     "img": "https://www.ofertaviva.com.br/photo/casuais-dois-botoes-dos-homens-stand-up-colarinho-terno-da-moda-plus-size-casaco-do-terno-de-negocio.jpg",
-    "price": "5 800"
+    "price": "5800"
   },
   {
     "id": 4,
     "name": "Ternos Jaqueta De Caminhadas",
     "img": "https://www.ofertaviva.com.br/photo/ternos-jaqueta-de-caminhadas-ao-ar-livre-homens-plus-size-blusao-de-secagem-rapida-a-prova-d-agua-mulheres-ternos-jaqueta-de-pes.jpg",
-    "price": "7 400"
+    "price": "7400"
   },
   {
     "id": 5,
     "name": "Moda Homem Respirável Apontou",
     "img": "https://www.ofertaviva.com.br/photo/moda-homem-respiravel-apontou-toe-sapatos-de-couro-homens-lace-up-oxfords-sapatos-para-homens-de-estilo-britanico-homens-sapatos.jpg",
-    "price": "1 500"
+    "price": "1500"
   },
   {
     "id": 6,
     "name": "Мужские повседневные туфли",
     "img": "https://ae04.alicdn.com/kf/H4c0a61040c5b49c2a71188258932a0925/-.jpg",
-    "price": "2 500"
+    "price": "2500"
   }
 ]
 
 const App = () => {
 
-  const [name, setName] = useState();
+  const [cart, setCart] = useState({
+    qty: 0,
+    price: 0
+  });
   const {tg, user} = useTelegram();
 
   useEffect(() => {
     tg.ready();
   }, [tg])
 
-  useEffect(() => {
-    setName(
-      JSON.stringify(user)
-    )
-  }, [user])
+  const onAddToCart = (item) => { 
+
+    setCart({
+      ...cart,
+      qty: cart.qty + 1,
+      price: cart.price + item.price
+    })
+
+    if(cart.qty > 0) {
+      tg.MainButton.show();
+      tg.MainButton.setParams({
+        text: `Купить ${cart.price} (${cart.qty})`
+      });
+    } else {
+      tg.MainButton.hide();
+    }
+  
+  }
 
   return (
-    <div className="App">
+    <div className="App mt-2">
         <div className='container'>
           <div className='row'>
-            {goods.map(item => {
-                return <Item item={item}/>
-              })}
+            {
+              goods.map(item => {
+                return <Item onAddToCart={onAddToCart} item={item}/>
+              })
+            }
           </div>
         </div>
     </div>
--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,15 +1,19 @@
-import { Link } from "react-router-dom";
 
 
-const Item = ({item}) => {
+const Item = ({item, onAddToCart}) => {
     return (
         <div className='col col-md-6 col-6' key={item.id}>
             <div className="card mb-4">
                 <img src={item.img} className="card-img-top img-fluid" alt="..."/>
                 <div className="card-body">
-                <h6 className="card-title">{item.name}</h6>
-                <p className="card-text">{item.price}<small>₽</small></p>
-                <Link to={() => console.log('test')} className="btn btn-warning" style={{width: "100%"}}>Купить</Link>
+                    <h6 className="card-title">{item.name}</h6>
+                    <p className="card-text">{item.price}<small>₽</small></p>
+                    <button
+                        onClick={() => onAddToCart(item)}
+                        className="btn btn-warning" 
+                        style={{width: "100%"}}>
+                        Купить
+                    </button>
                 </div>
             </div>
         </div>
