44b0b081d94071917c7f74f15871f4f46d19f5b6
Borz <bubaev.d@roistat.com>
2022-10-15 02:09:44 +0300

    Новое обновление

--- src/Firebase/FirebaseApi.js
+++ src/Firebase/FirebaseApi.js
@@ -1,4 +1,4 @@
-import { collection, getDocs, getDoc, doc } from "firebase/firestore";
+import { collection, getDocs, getDoc, doc } from "@firebase/firestore";
 import { firestore } from "./FirebaseConfig";
 
 const PRODUCTS_TABLE = 'products';
